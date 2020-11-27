b27378a90b0beb796ea427279b034a8f0a0d7374
Aleksandra Zobnina <azobnina@neoflex.ru>
2020-11-27 19:57:54 +0300

    traversing

--- src/main/java/org/example/tree/traversing/TraversalInDepth.java
+++ src/main/java/org/example/tree/traversing/TraversalInDepth.java
@@ -1,11 +1,26 @@
 package org.example.tree.traversing;
 
 import org.example.tree.Node;
+
 import java.util.ArrayList;
 
-public class TraversalInDepth<T> implements TreeTraversal<T>{
+public class TraversalInDepth<T> implements TreeTraversal<T> {
     @Override
     public ArrayList<Node<T>> traverse(Node<T> root) {
-        return null;
+        ArrayList<Node<T>> nodes = new ArrayList<>();
+        ArrayList<Node<T>> temp = new ArrayList<>();
+        temp.add(root);
+        nodes.add(root);
+        while (temp.size() != 0) {
+            Node<T> current = temp.get(0);
+            temp.remove(0);
+            temp.addAll(current.getChildren());
+            int tempIndex = nodes.indexOf(current) + 1;
+            for (Node<T> node : current.getChildren()) {
+                nodes.add(tempIndex, node);
+                tempIndex++;
+            }
+        }
+        return nodes;
     }
 }
--- src/main/java/org/example/tree/traversing/TraversalInWidth.java
+++ src/main/java/org/example/tree/traversing/TraversalInWidth.java
@@ -7,6 +7,17 @@ import java.util.ArrayList;
 public class TraversalInWidth<T> implements TreeTraversal<T> {
     @Override
     public ArrayList<Node<T>> traverse(Node<T> root) {
-        return null;
+        if (root == null) return new ArrayList<>();
+        ArrayList<Node<T>> nodes = new ArrayList<>();
+        ArrayList<Node<T>> temp = new ArrayList<>();
+        nodes.add(root);
+        temp.add(root);
+        while (temp.size() != 0) {
+            Node<T> current = temp.get(0);
+            temp.remove(0);
+            nodes.addAll(current.getChildren());
+            temp.addAll(current.getChildren());
+        }
+        return nodes;
     }
 }
