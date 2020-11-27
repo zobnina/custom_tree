9b7ee185f5fea2caa0d593714a995feb8c7a681a
Aleksandra Zobnina <azobnina@neoflex.ru>
2020-11-27 19:53:42 +0300

    implementation of tree

--- src/main/java/org/example/tree/CommonTree.java
+++ src/main/java/org/example/tree/CommonTree.java
@@ -1,6 +1,7 @@
 package org.example.tree;
 
 import org.example.tree.traversing.TraversableTree;
+import org.example.tree.traversing.TraversalInDepth;
 import org.example.tree.traversing.TreeTraversal;
 
 import java.util.ArrayList;
@@ -11,39 +12,82 @@ public class CommonTree<T> implements Tree<T>, Iterable<T>, TraversableTree<T> {
     private TreeTraversal<T> traversal;
 
     public CommonTree() {
-
+        root = null;
+        traversal = new TraversalInDepth<>();
     }
 
     public CommonTree(TreeTraversal<T> traversal) {
         this.traversal = traversal;
     }
 
+    @Override
     public Iterator<T> iterator() {
-        return null;
+        return new TreeIterator();
     }
 
+    @Override
     public Node<T> getRoot() {
-        return null;
+        return root;
     }
 
+    @Override
     public void setRoot(T value) {
-
+        this.root = new CommonNode(value);
     }
 
+    @Override
     public Node<T> add(Node<T> parent, T value) {
+        if (root == null) setRoot(parent.getData());
+        ArrayList<Node<T>> nodes = traversal.traverse(root);
+        if (nodes.contains(parent)) {
+            CommonNode newNode = new CommonNode(value);
+            parent.getChildren().add(newNode);
+            return newNode;
+        }
         return null;
     }
 
+    @Override
     public boolean remove(Node<T> node) {
+        if (root == node) setRoot(null);
+        ArrayList<Node<T>> nodes = traversal.traverse(root);
+        for (Node<T> n : nodes
+        ) {
+            if (n.getChildren().contains(node)) return n.getChildren().remove(node);
+        }
         return false;
     }
 
+    @Override
     public TreeTraversal<T> getTraversal() {
-        return null;
+        return traversal;
     }
 
+    @Override
     public void setTraversal(TreeTraversal<T> traversal) {
+        this.traversal = traversal;
+    }
 
+    class TreeIterator implements Iterator<T>{
+        private CommonNode current;
+
+        public TreeIterator() {
+            current = root;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public T next() {
+            T data = current.data;
+            ArrayList<Node<T>> nodes = traversal.traverse(root);
+            int currentIndex = nodes.indexOf(current);
+            current = currentIndex < nodes.size() - 1 ? (CommonNode) nodes.get(currentIndex + 1) : null;
+            return data;
+        }
     }
 
     class CommonNode implements Node<T>{
