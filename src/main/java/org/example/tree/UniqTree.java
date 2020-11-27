3ad4cb8b9505fbeebc16e5867d1b8026b3aaecc2
Aleksandra Zobnina <azobnina@neoflex.ru>
2020-11-27 19:57:44 +0300

    uniq tree

--- src/main/java/org/example/tree/UniqTree.java
+++ src/main/java/org/example/tree/UniqTree.java
@@ -6,38 +6,47 @@ import org.example.tree.traversing.TreeTraversal;
 import java.util.Iterator;
 
 public class UniqTree<T> implements Iterable<T>, Tree<T>, TraversableTree<T> {
-    @Override
-    public Iterator<T> iterator() {
-        return null;
+    private final CommonTree<T> tree;
+
+    public UniqTree(CommonTree<T> tree) {
+        this.tree = tree;
     }
 
     @Override
     public Node<T> getRoot() {
-        return null;
+        return tree.getRoot();
     }
 
     @Override
     public void setRoot(T value) {
-
+        tree.setRoot(value);
     }
 
     @Override
     public Node<T> add(Node<T> parent, T value) {
-        return null;
+        for (T element : tree) {
+            if (element.equals(value)) return null;
+        }
+        return tree.add(parent, value);
     }
 
     @Override
     public boolean remove(Node<T> node) {
-        return false;
+        return tree.remove(node);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return tree.iterator();
     }
 
     @Override
     public TreeTraversal<T> getTraversal() {
-        return null;
+        return tree.getTraversal();
     }
 
     @Override
     public void setTraversal(TreeTraversal<T> traversal) {
-
+        tree.setTraversal(traversal);
     }
 }
